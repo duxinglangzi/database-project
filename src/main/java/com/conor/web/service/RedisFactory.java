5c787b3db164e537ab51a540bbc4f4bc64f69d22
duxinglangzi <871364441@qq.com>
2021-06-08 16:25:02 +0800

    增加部分注释

--- src/main/java/com/conor/web/service/RedisFactory.java
+++ src/main/java/com/conor/web/service/RedisFactory.java
@@ -15,6 +15,12 @@ public class RedisFactory {
     @Resource
     private List<AbsRedisClientService> absRedisClientService;
 
+    /**
+     * <p> 获取数据库 </p>
+     * @param database 数据编号 0(默认)、1、2、3
+     * @return com.conor.web.service.AbsRedisClientService
+     * @author conor 2021-06-08 16:21
+     */
     public AbsRedisClientService getRedisByDatabase(int database) {
         for (AbsRedisClientService clientService : absRedisClientService) {
             if (clientService.getDatabase() == database) return clientService;
@@ -22,18 +28,38 @@ public class RedisFactory {
         return getDefaultRedis();
     }
 
+    /**
+     * <p> 连接默认数据 </p>
+     * @return com.conor.web.service.AbsRedisClientService
+     * @author conor 2021-06-08 16:20
+     */
     public AbsRedisClientService getDefaultRedis() {
         return getRedisByDatabase(0);
     }
 
+    /**
+     * <p> 连接数据 1  </p>
+     * @return com.conor.web.service.AbsRedisClientService
+     * @author conor 2021-06-08 16:20
+     */
     public AbsRedisClientService getRedis1() {
         return getRedisByDatabase(1);
     }
 
+    /**
+     * <p> 连接数据 2 </p>
+     * @return com.conor.web.service.AbsRedisClientService
+     * @author conor 2021-06-08 16:20
+     */
     public AbsRedisClientService getRedis2() {
         return getRedisByDatabase(2);
     }
 
+    /**
+     * <p> 连接数据 3  </p>
+     * @return com.conor.web.service.AbsRedisClientService
+     * @author conor 2021-06-08 16:20
+     */
     public AbsRedisClientService getRedis3() {
         return getRedisByDatabase(3);
     }
